98f28246b9a01eca1c43c31daabb8fe692666371
“caozongkai1008” <czk_971008@163.com>
2019-05-07 23:32:32 +0800

    RBTree-iterator

--- test_4_23/map_test.cpp
+++ test_4_23/map_test.cpp
@@ -1,16 +1,36 @@
 #include <map>
 #include <iostream>
 using namespace std;
+#if 0
+int main()
+{
+  
+  map<int,string,greater<int>> stringmap;
+  map<int,string>::const_iterator it;
+  stringmap[2] = "天蓬元帅";
+  stringmap[1] = "齐天大圣";
+  it = stringmap.find(1);
+  //find函数返回值是迭代器
+  stringmap.insert(make_pair<int,string>(3,"阿弥陀佛"));
+  for(auto e : stringmap)
+  {
+    cout<<e.first<<":"<<e.second<<endl;
+  }
+  return 0;
+}
+#endif
 
 int main()
 {
-  map<string,string> stringmap;
-  map<string,string>::const_iterator it;
-  stringmap["孙悟空"] = "齐天大圣";
-  stringmap["猪八戒"] = "天蓬元帅";
-  it = stringmap.find("猪八戒");
-  cout<<stringmap.find("猪八戒")->second<<endl;
+  map<string,string> m;
+  m["apple"] = "苹果";
+  m["apple"] = "小苹果";
+  m["apple"] = "小苹果";
+  for(auto e = m.begin();e != m.end();++e)
+  {
+    cout<<e->first<<"-->"<<e->second<<endl;
+  }
+  m.insert(pair<string,string>("banana","香蕉"));
+  cout<<m.at("banana")<<endl;
   return 0;
 }
-
-
