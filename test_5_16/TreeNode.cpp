6431d0b1605a9cf22989899d8fc990e8b7ce25e5
“caozongkai1008” <czk_971008@163.com>
2019-06-14 21:42:50 +0800

    TreeNode

--- test_5_16/TreeNode.cpp
+++ test_5_16/TreeNode.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <queue>
 #include <stack>
 #include <stdlib.h>
 using namespace std;
@@ -38,6 +39,51 @@ public:
         }
       }
       return res;
+    }
+private:
+    vector<int> res;
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution1 {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        if(root == NULL)
+        {
+            return {};
+        }
+        while(!q.empty())
+        {
+            int size = q.size();
+            while(size--)
+            {
+                TreeNode* tmp = q.front();
+                q.pop();
+                if(0 == size)
+                {
+                    res.push_back(tmp->val);
+                }
+                if(tmp->left)
+                {
+                    q.push(tmp->left);
+                }
+                if(tmp->right)
+                {
+                    q.push(tmp->right);
+                }
+            }
+        }
+        return res;
     }
 private:
     vector<int> res;
