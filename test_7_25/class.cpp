03529049e2b4f349db464c9dd4be9d754a806722
“caozongkai1008” <czk_971008@163.com>
2019-07-25 20:56:31 +0800

    内部类

--- test_7_25/class.cpp
+++ test_7_25/class.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+#if 0
 class Date
 {
   public:
@@ -19,4 +22,49 @@ void Time(Date d)
 {
   d._year = 1;
   d.s = 2;
+}
+#endif
+
+class Date
+{
+  public:
+    Date(int year = 100,int month = 10,int day = 10)
+      :_year(year)
+      ,_month(month)
+      ,_day(day)
+    {}
+
+    class Inside 
+    {
+      public:
+        Inside(int iyear = 1,int imonth = 1,int iday = 1)
+        {
+          _iyear = iyear;
+          _imonth = imonth;
+          _iday = iday;
+        }
+
+        void Print(Date d)
+        {
+          cout << "year:" << d._year << "month:" << d._month << "day:" << d._day<< endl;  
+        }
+      private:
+        int _iyear;
+        int _imonth;
+        int _iday;
+    };
+
+  private:
+    int _year;
+    int _month;
+    int _day;
+};
+
+int main()
+{
+  Date::Inside i;
+  Date d;
+  
+  i.Print(d);
+  return 0;
 }
