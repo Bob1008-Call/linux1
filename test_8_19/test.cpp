50347caa22fcc5e71ed6d256bb068aff8722fb6a
“caozongkai1008” <czk_971008@163.com>
2019-08-19 23:49:10 +0800

    leetcode

---
+++ test_8_19/test.cpp
@@ -0,0 +1,30 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string convert(string s, int numRows) {  
+        int length=s.length();
+        if(numRows>=length||numRows<2) return s;
+        string ans;
+        for( int row=0;row<numRows;row++){
+            int i=row;
+            if(row==0||row==numRows-1){
+                while(i<length){
+                    ans+=s[i];
+                    i+=numRows*2-2;
+                }
+            }
+            else{
+                while(i<length){
+                 ans+=s[i];
+                 i+=(numRows-row-1)*2;
+                    if(i<length){
+                        ans+=s[i];
+                        i+=row*2;
+                    }
+                }
+            }
+        }
+        return ans;     
+    }
+};
